0da354ea5db31f1b4fda919cf04114d7376fd868
Pi-Yueh Chuang <pychuang@gwu.edu>
2018-02-07 19:10:42 -0500

    re-organize tests/basic and add tests for to_coefficients

--- tests/basic/operations1.cpp
+++ tests/basic/operations1.cpp
@@ -1,6 +1,6 @@
 /**
- * \file add.cpp
- * \brief Unit tests for polynomial addition.
+ * \file operations1.cpp
+ * \brief Unit tests for polynomial basic operations.
  * \author Pi-Yueh Chuang
  * \version beta
  * \date 2018-01-29
@@ -17,6 +17,93 @@
 extern std::default_random_engine generator;
 
 using namespace simpoly;
+
+
+TEST(PolynomialCoeffs, GetCoeffsFromRoots1)
+{
+    basic::DArry roots({
+            0.8538721859301963146,  0.2921220802626576241,
+            -0.9125881264026460826,  0.2467728385548548786,
+            -0.4448851063326948463, -0.6172124325075281082,
+            -0.6301262014641872966});
+    
+    basic::DArry expect({
+            -0.0097194008368678246,  0.0203713227801400679,
+            0.1774520322384245952, -0.1187869060899048868,
+            -1.1062246986215185096, -0.6075137057194068824,
+            1.2120447619593475164,  1.});
+
+    basic::DArry result = basic::to_coefficients(1.0, roots);
+
+    ASSERT_EQ(expect.size(), result.size());
+    
+    for(unsigned i=0; i<expect.size(); ++i)
+        ASSERT_NEAR(expect[i], result[i], 1e-10);
+}
+
+
+static auto f = [](basic::Cmplx i, basic::Cmplx j)->bool {
+    if (std::abs(std::abs(i)-std::abs(j)) <= 1e-11)
+    {
+        if (std::abs(i.real()-j.real()) <= 1e-11)
+            if (i.imag() <= j.imag()) return true;
+        
+        if ((i.real()-j.real()) < -1e-11) return true;
+    }
+    if ((std::abs(i)-std::abs(j)) < -1e-11) return true;
+    return false;
+};
+TEST(PolynomialCoeffs, GetCoeffsFromRoots2)
+{
+    basic::CArry roots({
+            basic::Cmplx(0.0523249771240981154, 0.7007289936213532133),
+            basic::Cmplx(-0.1511969052827171200, 0.4108412398766685403),
+            basic::Cmplx(0.7769313311349028695, 0.7301249050268194818),
+            basic::Cmplx(0.0046666287791925498, 0.9696847924026805732),
+            basic::Cmplx(-0.5789703445402629711, -0.6424983236922838792),
+            basic::Cmplx(-0.8725637786525710649, -0.2471912341041830885),
+            basic::Cmplx(-0.8482223096380121508, 0.1376940842539751131),
+            basic::Cmplx(0.0332852932796565693, -0.8528866044880742958),
+            basic::Cmplx(0.8522720871683582278, 0.1953603750595562794),
+            basic::Cmplx(-0.8680725141564455960, 0.423548363231143199) ,
+            basic::Cmplx(0.8824640585760064049, -0.7545152381037976141),
+            basic::Cmplx(0.8001160654174328535, 0.5327952053290956336),
+            basic::Cmplx(0.7524249058508991617, -0.4850694648111775198),
+            basic::Cmplx(0.4670358901871698443, 0.7246115099814800509),
+            basic::Cmplx(0.6425671238360994852, -0.8227184096042670092),
+            basic::Cmplx(-0.8746249058224726536, 0.4526170928635313562),
+            basic::Cmplx(-0.8405665222993630170, 0.4624841604488800773)});
+    
+    basic::CArry expect({
+            basic::Cmplx(0.1284473674676091248, 0.027106308015679835) ,
+            basic::Cmplx(-0.0055811935483588704, 0.5895676656919293102),
+            basic::Cmplx(-1.0697675991580708477, 0.0739888122261974646),
+            basic::Cmplx(0.0350214941923967515, -1.2581110962554780119),
+            basic::Cmplx(1.1311480794477093426, 0.5697505628564978331),
+            basic::Cmplx(-0.9578963956878190356, 0.8762473636503139707),
+            basic::Cmplx(-0.7142421431728066539, -0.9120235859686047775),
+            basic::Cmplx(0.6191826581229090687, 0.1287334984037127317),
+            basic::Cmplx(-1.0386382947640331320, 0.6191766562897030868),
+            basic::Cmplx(-1.4622204324047241020, -2.0911509295099053318),
+            basic::Cmplx(2.5643980136130921643, -1.3562694162887596327),
+            basic::Cmplx(0.6477469434997339404, 3.3575574217272889932),
+            basic::Cmplx(-4.6940592271922572110, -0.9211194673159723845),
+            basic::Cmplx(1.7891645824502104833, -4.8182053599055656434),
+            basic::Cmplx(3.5022002736720923011, 2.2497280480793735435),
+            basic::Cmplx(-2.7137599509433454337, 1.2366520250938943803),
+            basic::Cmplx(-0.2298710809619715079, -1.9356114472913998892),
+            basic::Cmplx(1.0000000000000000000, 0.)});
+
+    basic::CArry result = basic::to_coefficients(basic::Cmplx(1.0), roots);
+
+    ASSERT_EQ(expect.size(), result.size());
+    
+    for(unsigned i=0; i<expect.size(); ++i)
+    {
+        ASSERT_NEAR(expect[i].real(), result[i].real(), 1e-10);
+        ASSERT_NEAR(expect[i].imag(), result[i].imag(), 1e-10);
+    }
+}
 
 
 TEST(PolynomialAdd, EqualLength)
@@ -77,6 +164,7 @@ TEST(PolynomialAdd, SecondOneLonger)
     for(unsigned i=0; i<p2.size(); ++i) ASSERT_NEAR(p1[i]+p2[i], result[i], 1e-12);
     for(unsigned i=p2.size(); i<len; ++i) ASSERT_NEAR(p1[i], result[i], 1e-12);
 }
+
 
 TEST(PolynomialGCD, GCD1)
 {
--- tests/basic/operations2.cpp
+++ tests/basic/operations2.cpp
@@ -1,6 +1,6 @@
 /**
- * \file multiply.cpp
- * \brief Unit tests for polynomial multiplication.
+ * \file operators2.cpp
+ * \brief Unit tests for polynomial basic operations.
  * \author Pi-Yueh Chuang
  * \version beta
  * \date 2018-01-29
@@ -123,4 +123,109 @@ TEST(PolynomialMult, RandomPolynomials)
         }
         ASSERT_NEAR(value, actual[i], 1e-12);
     }
+}
+
+
+# ifndef NDEBUG
+TEST(PolynomialDivide, ZeroLengthTests)
+{
+    std::valarray<double> p1(0), p2(1);
+    
+    try { basic::divide(p1, p2); }
+    catch (exceptions::ZeroCoeffsLength e) {};
+    
+    try { basic::divide(p2, p1); }
+    catch (exceptions::ZeroCoeffsLength e) {};
+}
+# endif
+
+TEST(PolynomialDivide, LongerSecondPoly)
+{
+    std::valarray<double> p1({1.0, 2.0, 3.0}), p2({1.0, 2.0, 3.0, 4.0});
+    
+    // test the version without remainder
+    std::valarray<double> q = basic::divide(p1, p2);
+    ASSERT_EQ(1, q.size());
+    ASSERT_DOUBLE_EQ(0.0, q[0]);
+    
+    // test the version with remainder
+    std::valarray<double> r;
+    q = basic::divide(p1, p2, r);
+    ASSERT_EQ(1, q.size());
+    ASSERT_DOUBLE_EQ(0.0, q[0]);
+    for(unsigned i=0; i<3; ++i) ASSERT_DOUBLE_EQ(p1[i], r[i]);
+}
+
+TEST(PolynomialDivide, FixPolynomial1)
+{
+    std::valarray<double> p1({-10.0, -9.0, 1.0}), p2({1.0, 1.0});
+    std::valarray<double> q, r;
+    
+    q = basic::divide(p1, p2, r);
+    
+    ASSERT_EQ(2, q.size());
+    ASSERT_DOUBLE_EQ(-10.0, q[0]);
+    ASSERT_DOUBLE_EQ(1.0, q[1]);
+    
+    ASSERT_EQ(1, r.size());
+    ASSERT_DOUBLE_EQ(0.0, r[0]);
+}
+
+TEST(PolynomialDivide, FixPolynomial2)
+{
+    std::valarray<double> p1({-3.0, 10.0, -5.0, 3.0}), p2({1.0, 3.0});
+    std::valarray<double> q, r;
+    
+    q = basic::divide(p1, p2, r);
+    
+    ASSERT_EQ(3, q.size());
+    ASSERT_DOUBLE_EQ(4.0, q[0]);
+    ASSERT_DOUBLE_EQ(-2.0, q[1]);
+    ASSERT_DOUBLE_EQ(1.0, q[2]);
+    
+    ASSERT_EQ(1, r.size());
+    ASSERT_DOUBLE_EQ(-7.0, r[0]);
+}
+
+TEST(PolynomialDivide, FixPolynomial3)
+{
+    std::valarray<double> p1({1.0, 2.0, 0.0, 3.0, 4.0}), p2({2.0, 1.0, 1.0});
+    std::valarray<double> q, r;
+    
+    q = basic::divide(p1, p2, r);
+    
+    ASSERT_EQ(3, q.size());
+    ASSERT_DOUBLE_EQ(-7.0, q[0]);
+    ASSERT_DOUBLE_EQ(-1.0, q[1]);
+    ASSERT_DOUBLE_EQ(4.0, q[2]);
+    
+    ASSERT_EQ(2, r.size());
+    ASSERT_DOUBLE_EQ(15.0, r[0]);
+    ASSERT_DOUBLE_EQ(11.0, r[1]);
+}
+
+TEST(PolynomialDivide, RandomPolynomial)
+{
+    std::uniform_int_distribution<int> dist1(1, 20);
+    int len1 = dist1(generator);
+    
+    std::uniform_int_distribution<int> dist2(1, len1);
+    int len2 = dist2(generator);
+    
+    std::valarray<double> p1(len1), p2(len2);
+    std::uniform_real_distribution<double> rdist(1, 2);
+    std::uniform_int_distribution<int> bdist(0, 1);
+    for(unsigned i=0; i<len1; ++i) 
+        p1[i] = rdist(generator)*((bdist(generator))? 1.0 : -1.0);
+    for(unsigned i=0; i<len2; ++i) 
+        p2[i] = rdist(generator)*((bdist(generator))? 1.0 : -1.0);
+    
+    std::valarray<double> q, r, expect;
+    
+    q = basic::divide(p1, p2, r);
+    
+    expect = basic::add(basic::multiply(p2, q), r);
+    ASSERT_EQ(len1, expect.size());
+    
+    for(unsigned i=0; i<len1; ++i) ASSERT_NEAR(p1[i], expect[i], 1e-8);
 }
